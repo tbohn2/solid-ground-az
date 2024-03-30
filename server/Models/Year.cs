0286978fcba7b2016d1c235ed605e105ce45f838
tbohn2 <tanner.bohn@yahoo.com>
2024-03-29 17:59:09 -0700

    Method to add month

--- server/Models/Year.cs
+++ server/Models/Year.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 
 namespace StretchScheduler.Models
 {
-    public class Year
+    public class Year(int yearNumber)
     {
         public int Id { get; set; }
-        public required int YearNumber { get; set; }
-        public List<Month>? Months { get; set; }
+        public required int YearNumber { get; set; } = yearNumber;
+        public List<Month>? Months { get; set; } = new List<Month>();
+
+        // Method to add a Month to the Year
+        public void AddMonth(Month month)
+        {
+            Months?.Add(month);
+        }
     }
 }
