87c7f0377aaccb97d37e8675e7bd37cace1629b3
tbohn2 <tanner.bohn@yahoo.com>
2024-04-02 20:56:56 -0700

    Changed Time model to Appointment

--- server/Models/Appointment.cs
+++ server/Models/Appointment.cs
@@ -2,15 +2,17 @@ using System;
 
 namespace StretchScheduler.Models
 {
-    public class Time
+    public class Appointment
     {
         public int Id { get; set; }
+        public required string Type { get; set; }
         public required string TimeOfDay { get; set; }
+        public required int Duration { get; set; }
         public bool Booked { get; set; } = false;
         public bool Requested { get; set; } = false;
         // public int ClientId { get; set; }
         // public Client Client { get; set; }
-        public int DateNumber { get; set; }
-        public required Date Date { get; set; }
+        public required int DateId { get; set; } // Foreign key
+        public Date? Date { get; set; }
     }
 }
