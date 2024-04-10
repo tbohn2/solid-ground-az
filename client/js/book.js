ef64572d206f94a14d3ecf45983d287813f58e5f
tbohn2 <tanner.bohn@yahoo.com>
2024-04-09 19:28:57 -0700

    Modal shows available appts in day; client can choose type of appointment; styled modal

--- client/js/book.js
+++ client/js/book.js
@@ -4,13 +4,13 @@ const currentYear = new Date().getFullYear();
 const currentMonth = new Date().getMonth() + 1;
 let displayedYear = currentYear;
 let displayedMonth = currentMonth;
-let availableAppts = [];
+let appointments = [];
 
 async function getAppointments() {
     try {
         const response = await fetch(`http://localhost:5062/api/apptsInMonth/${displayedMonth}/${displayedYear}`);
         const data = await response.json();
-        availableAppts = data;
+        appointments = data;
     } catch (error) {
         console.error(error);
     }
@@ -22,7 +22,6 @@ let displayedDates = new calendar.Calendar(6).monthdayscalendar(displayedYear, d
 async function renderCalendar() {
     $('#month-year').text(`${months[displayedMonth - 1]} ${displayedYear}`);
     await getAppointments();
-    console.log(availableAppts);
 
     displayedDates.forEach(week => {
         let weekDisplay = $('<div class="d-flex"></div>');
@@ -33,12 +32,12 @@ async function renderCalendar() {
                 dayDisplay.text('');
             }
             else {
+                const availableApptsInMonth = appointments.filter(appt => appt.Date === day && appt.Requested === false && day >= currentDay);
                 if (day === currentDay && displayedMonth === currentMonth && displayedYear === currentYear) {
                     dayDisplay.addClass('currentDay');
                 }
-                if (availableAppts.find(appt => appt.Date === day && appt.Requested === false && day >= currentDay)) {
-                    console.log("yo");
-                    dayDisplay = $('<div class="date availableDate" data-bs-toggle="modal" data-bs-target="#serviceSelection"></div>');
+                if (availableApptsInMonth.length != 0) {
+                    dayDisplay = $(`<div id=${day} class="date availableDate" data-bs-toggle="modal" data-bs-target="#serviceSelection"></div>`);
                     dayDisplay.text(day);
                 } else {
                     dayDisplay.text(day);
@@ -49,7 +48,48 @@ async function renderCalendar() {
         });
         $('#calendar-dates').append(weekDisplay);
     });
+
+    $('.availableDate').on('click', (event) => {
+        $('#serviceSelectionLabel').text('Available Times');
+        const day = parseInt(event.target.id);
+        const availableApptsInDay = appointments.filter(appt => appt.Date === day && appt.Requested === false);
+
+        availableApptsInDay.forEach(appt => {
+            const timeDisplay = $(`<div class="col-5 m-1 text-center time-option">${appt.Time}</div>`);
+            $('#modal-body').append(timeDisplay);
+        });
+        $('.time-option').on('click', (event) => {
+            $('#serviceSelectionLabel').text('Select Service');
+            $('#modal-body').empty();
+
+            const time = event.target.innerText;
+            const selectedAppt = availableApptsInDay.find(appt => appt.Time === time);
+            const dropdown = $(` 
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle fs-3" type="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    Select Type
+                </button>
+                <ul class="dropdown-menu m-0 p-0">
+                    <li class="dropdown-item fs-3">$50 Private Yoga 60 min</li>
+                    <li class="dropdown-item fs-3">$20 Assisted Stretch 25 min</li>
+                    <li class="dropdown-item fs-3">$20 Assisted Stretch 50 min</li>
+                    <li class="dropdown-item fs-3">$50 Blended Service 60 min</li>
+                </ul>
+            </div>`);
+            $('#modal-body').append(dropdown);
+            $('.dropdown-item').on('click', (event) => {
+                $('.dropdown-toggle').text(event.target.innerText);
+            });
+            $('#modal-body').append(`<button class="monthNavBtn mt-3">Request Appointment</button>`);
+        });
+    });
 }
+
+$('[data-bs-dismiss="modal"]').on('click', () => {
+    $('#serviceSelectionLabel').empty();
+    $('#modal-body').empty();
+});
 
 $('#prev').on('click', () => {
     displayedMonth -= 1;
