0e47fd49449a8634acb3a40fb3862bfe6adebaa4
tbohn2 <tanner.bohn@yahoo.com>
2024-04-10 14:56:44 -0700

    Client request for an appointment is properly sent to server and saved

--- client/js/book.js
+++ client/js/book.js
@@ -50,20 +50,23 @@ async function renderCalendar() {
     });
 
     $('.availableDate').on('click', (event) => {
-        $('#serviceSelectionLabel').text('Available Times');
         const day = parseInt(event.target.id);
+        const dateDisplay = displayedMonth + '/' + day + '/' + displayedYear
+        $('#serviceSelectionLabel').text('Available on' + ' ' + displayedMonth + '/' + day + '/' + displayedYear);
         const availableApptsInDay = appointments.filter(appt => appt.Date === day && appt.Requested === false);
 
         availableApptsInDay.forEach(appt => {
-            const timeDisplay = $(`<div class="col-5 m-1 text-center time-option">${appt.Time}</div>`);
+            const timeDisplay = $(`<div id=${appt.Id} class="col-5 m-1 text-center time-option">${appt.Time}</div>`);
             $('#modal-body').append(timeDisplay);
         });
         $('.time-option').on('click', (event) => {
             $('#serviceSelectionLabel').text('Select Service');
             $('#modal-body').empty();
-
             const time = event.target.innerText;
+            $('#modal-body').append(`<div id=dateDisplay class="fs-3 m-1 text-center">${time} | ${dateDisplay}</div>`);
+
             const selectedAppt = availableApptsInDay.find(appt => appt.Time === time);
+            let selectedService
             const dropdown = $(` 
             <div class="dropdown">
                 <button class="btn btn-secondary dropdown-toggle fs-3" type="button" data-bs-toggle="dropdown"
@@ -79,9 +82,79 @@ async function renderCalendar() {
             </div>`);
             $('#modal-body').append(dropdown);
             $('.dropdown-item').on('click', (event) => {
-                $('.dropdown-toggle').text(event.target.innerText);
+                selectedService = event.target.innerText;
+                $('.dropdown-toggle').text(selectedService);
             });
-            $('#modal-body').append(`<button class="monthNavBtn mt-3">Request Appointment</button>`);
+            $('#modal-body').append(`<button id=RequestBtn class="monthNavBtn mt-3">Request Appointment</button>`);
+            $('#RequestBtn').on('click', () => {
+                $('#serviceSelectionLabel').text('Enter Information');
+                $('#modal-body').empty();
+                $('#modal-body').append(`<div id=dateDisplay class="fs-3 m-1 text-center">${time} | ${dateDisplay}</div>`);
+                $('#modal-body').append(`<div id=dateDisplay class="fs-3 m-1 text-center">${selectedService}</div>`);
+
+                const form = $(
+                    `<form class="d-flex flex-column justify-content-between">
+                        <label for="nameInput" class="form-label">Name</label>
+                        <input type="text" class="form-control mb-3" id="nameInput" required>
+                        <label for="emailInput" class="form-label">Email address</label>
+                        <input type="email" class="form-control mb-3" id="emailInput" required>
+                        <label for="phoneInput" class="form-label">Phone Number (10 digits)</label>
+                        <input type="text" class="form-control mb-3" id="phoneInput"
+                            pattern="[0-9]{10}|[0-9]{3}-[0-9]{3}-[0-9]{4}" required>
+                        <button type="submit" class="monthNavBtn">Confirm Request</button>
+                   </form>`
+                )
+                $('#modal-body').append(form);
+                form.on('submit', async (event) => {
+                    event.preventDefault();
+                    $('#modal-body').empty()
+                    $('#modal-body').append(`
+                    <div class="spinner-border" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </div>`)
+
+                    const name = $('#nameInput').val();
+                    const email = $('#emailInput').val();
+                    const phone = $('#phoneInput').val();
+                    selectedService = selectedService.split(' ');
+
+                    const price = parseInt(selectedService[0].slice(1))
+                    const duration = parseInt(selectedService[selectedService.length - 2] + ' ' + selectedService[selectedService.length - 1]);
+                    let type = '';
+                    for (let i = 0; i < selectedService.length; i++) {
+                        if (!selectedService[i].includes('min') && !selectedService[i].match(/[0 - 9]/)) {
+                            type += ' ' + selectedService[i];
+                        }
+                    }
+
+                    const apptToRequest = {
+                        Id: selectedAppt.Id,
+                        Name: name,
+                        Email: email,
+                        Phone: phone,
+                        Type: type,
+                        Price: price,
+                        Duration: duration,
+                    }
+                    try {
+                        const response = await fetch('http://localhost:5062/api/requestAppt', {
+                            method: 'PUT',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify(apptToRequest)
+                        })
+                        if (response) {
+                            console.log('Success:', response);
+                            $('#serviceSelectionLabel').text('Appointment Requested');
+                            $('#modal-body').empty();
+                            $('#modal-body').append(`<div class="fs-3 m-1 text-center">Thank you for your request! Expect a response within 24 hours</div>`);
+                        }
+                    } catch (error) {
+                        console.error('Error:', error);
+                    }
+                });
+            });
         });
     });
 }
