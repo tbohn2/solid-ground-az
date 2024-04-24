f7c675cbfb65999792206251f18575107a45135e
tbohn2 <tanner.bohn@yahoo.com>
2024-04-23 17:23:15 -0700

    Consolidated date/time properties to one DateTime instance; added status property to replace individual Booked, Requested, etc. properties

--- server/Models/Appointment.cs
+++ server/Models/Appointment.cs
@@ -6,13 +6,15 @@ namespace StretchScheduler.Models
         public string? Type { get; set; }
         public int? Duration { get; set; }
         public int? Price { get; set; }
-        public required string Time { get; set; }
-        public required int Date { get; set; }
-        public required int Month { get; set; }
-        public required int Year { get; set; }
-        public bool Booked { get; set; } = false;
-        public bool Requested { get; set; } = false;
-        public bool Completed { get; set; } = false;
+        public required DateTime DateTime { get; set; } //Format: "yyyy-MM-dd HH:mm:ss"
+        public enum StatusOptions
+        {
+            Available,
+            Requested,
+            Booked,
+            Completed
+        }
+        public StatusOptions Status { get; set; } = StatusOptions.Available;
         public int? ClientId { get; set; }
         public Client? Client { get; set; }
     }
