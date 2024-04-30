d1ee6be08ab8814889503f844e150a44ea141bfa
tbohn2 <tanner.bohn@yahoo.com>
2024-04-29 20:56:59 -0700

    Removed client side

--- client/js/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('input[name="navOptions"]').change(
-        function () {
-            var pageToLoad = $(this).val();
-            window.location.assign(pageToLoad);
-        });
-});
