4576cec1e93b03c3ef8f09ac57373f4c4c41a02d
tbohn2 <tanner.bohn@yahoo.com>
2024-05-27 16:30:34 -0700

    Implemented placeholder images; changed page colors and styles

--- wwwroot/js/services.js
+++ wwwroot/js/services.js
@@ -4,28 +4,28 @@ const services = [
         description: 'Move through shapes and postures to benefit you individually',
         price: 50,
         time: 60,
-        imageURL: './assets/blueBG.jpg'
+        imageURL: './assets/services1.jpg'
     },
     {
         name: '25 MIN ASSISTED STRETCH',
         description: 'Customized assisted stretch',
         price: 20,
         time: 25,
-        imageURL: './assets/blueBG.jpg'
+        imageURL: './assets/services2.jpg'
     },
     {
         name: '50 MIN ASSISTED STRETCH',
         description: 'Customized assisted stretch',
         price: 40,
         time: 50,
-        imageURL: './assets/blueBG.jpg'
+        imageURL: './assets/services3.jpg'
     },
     {
         name: 'BLENDED SERVICE',
         description: 'Combined assisted stretch, ball rolling, and yoga',
         price: 50,
         time: 60,
-        imageURL: './assets/blueBG.jpg'
+        imageURL: './assets/services4.jpg'
     },
 ]
 
@@ -47,7 +47,7 @@ for (let i = 0; i < services.length; i++) {
     if (screenSize === 'mobile') {
         card = `
         <div class="serviceCard my-3 col-10 d-flex flex-column fade-top">
-            <img class="col-12" src="./assets/blueBG.jpg" alt="blue water">
+            <img class="col-12" src="${services[i].imageURL}" alt="yoga">
                 <h3 class="mt-3 align-self-center text-center">${services[i].name}</h3>
                 <p class="m-2 fs-5 align-self-center">${services[i].description}</p>
                 <div class="d-flex align-items-center justify-content-between col-12">
@@ -59,7 +59,7 @@ for (let i = 0; i < services.length; i++) {
     else {
         card = `
         <div class="serviceCard my-3 col-10 col-lg-5 d-flex fade-top">
-            <img class="col-5" src="./assets/blueBG.jpg" alt="blue water">
+            <img class="col-5" loading="lazy" src="${services[i].imageURL}" alt="yoga">
             <div class="col-7 d-flex flex-column align-items-start justify-content-between">
                 <h3 class="mt-3 align-self-center text-center">${services[i].name}</h3>
                 <p class="m-2 fs-5 align-self-center">${services[i].description}</p>
