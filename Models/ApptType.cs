8ce996c19d4b727be53f61661d1c65d51200ac48
tbohn2 <tanner.bohn@yahoo.com>
2024-06-04 13:47:50 -0700

    Created Email model; Added Private key to ApptType

--- Models/ApptType.cs
+++ Models/ApptType.cs
@@ -3,11 +3,13 @@ namespace StretchScheduler.Models
     public class ApptType
     {
         public int Id { get; set; }
+        public required bool Private { get; set; }
         public required string Name { get; set; }
         public required int Duration { get; set; }
         public required int Price { get; set; }
         public required string Description { get; set; }
         public string? Location { get; set; }
+        public required string ImgURL { get; set; } = "./assets/services1.jpg";
         public required Guid AdminId { get; set; }
     }
 };
---
+++ Models/Email.cs
@@ -0,0 +1,11 @@
+namespace StretchScheduler.Models
+
+{
+    public class Email
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string EmailAddress { get; set; }
+        public required string Message { get; set; }
+    }
+}
