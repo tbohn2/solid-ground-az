0acfaf941203dee991a93ec2dbef59931c39151e
tbohn2 <tanner.bohn@yahoo.com>
2024-06-04 13:45:52 -0700

    Created routes for users to get services and to send message

--- Controllers/UserRoutes.cs
+++ Controllers/UserRoutes.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using StretchScheduler.Models;
 using static StretchScheduler.Models.Appointment;
+using System.Net;
+using System.Net.Mail;
 
 namespace StretchScheduler
 {
@@ -10,6 +12,8 @@ namespace StretchScheduler
         public static void MapEndpoints(IEndpointRouteBuilder endpoints)
         {
             endpoints.MapGet("/api/apptsInMonth/{month}/{year}", GetAppts);
+            endpoints.MapGet("/api/services", GetServices);
+            endpoints.MapPost("/api/sendEmail", SendEmail);
             endpoints.MapPut("/api/requestAppt", RequestAppt);
         }
         public static async Task WriteResponseAsync(HttpContext context, int statusCode, string contentType, object data)
@@ -56,6 +60,82 @@ namespace StretchScheduler
             {
                 Console.WriteLine($"An error occurred: {ex.Message}");
                 await WriteResponseAsync(context, 500, "application/json", "An error occurred while getting data");
+            }
+        }
+        private static async Task GetServices(HttpContext context)
+        {
+            try
+            {
+                using (var scope = context.RequestServices.CreateScope())
+                {
+                    var idString = Environment.GetEnvironmentVariable("ID");
+                    if (Guid.TryParse(idString, out Guid adminId))
+                    {
+                        var dbContext = scope.ServiceProvider.GetRequiredService<StretchSchedulerContext>();
+                        var services = await dbContext.ApptTypes.Where(a => a.AdminId == adminId).ToListAsync();
+                        if (services == null)
+                        {
+                            await WriteResponseAsync(context, 404, "application/json", "No services found");
+                            return;
+                        }
+                        else
+                        {
+                            await WriteResponseAsync(context, 200, "application/json", services);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                await WriteResponseAsync(context, 500, "application/json", "An error occurred while getting data");
+            }
+        }
+        private static async Task SendEmail(HttpContext context)
+        {
+            var requestBody = await new StreamReader(context.Request.Body).ReadToEndAsync();
+            try
+            {
+                var userEmail = JsonConvert.DeserializeObject<Email>(requestBody);
+                if (userEmail == null)
+                {
+                    await WriteResponseAsync(context, 400, "application/json", "Invalid data, please provide required email data");
+                    return;
+                }
+                var email = Environment.GetEnvironmentVariable("EMAIL");
+                var password = Environment.GetEnvironmentVariable("GPW");
+                if (email == null || password == null)
+                {
+                    await WriteResponseAsync(context, 500, "application/json", "Email credentials not found");
+                    return;
+                }
+
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+                smtpClient.Port = 587;
+                smtpClient.Credentials = new NetworkCredential(email, password);
+                smtpClient.EnableSsl = true;
+
+                MailMessage mailMessage = new MailMessage();
+                mailMessage.From = new MailAddress(userEmail.EmailAddress);
+                mailMessage.To.Add(email);
+                mailMessage.Subject = "New Message from " + userEmail.FirstName + " " + userEmail.LastName;
+                mailMessage.Body = userEmail.FirstName + " " + userEmail.LastName + " (" + userEmail.EmailAddress + ")" + " sent the following message: " + userEmail.Message;
+
+                try
+                {
+                    smtpClient.Send(mailMessage);
+                    Console.WriteLine("Email sent successfully.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Failed to send email: " + ex.Message);
+                    await context.Response.WriteAsync(ex.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                await WriteResponseAsync(context, 500, "application/json", "An error occurred while sending the email");
             }
         }
         private static async Task RequestAppt(HttpContext context)
