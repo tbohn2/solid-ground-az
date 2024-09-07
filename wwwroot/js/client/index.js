6082602eb8451d395d3dcd6ebfc2c6e424e2a770
tbohn2 <tanner.bohn@yahoo.com>
2024-09-06 18:20:02 -0700

    Corrected img url for services

--- wwwroot/js/client/index.js
+++ wwwroot/js/client/index.js
@@ -18,11 +18,10 @@ async function renderServices() {
 
     const serviceCards = privateServices.map(service => {
         let card;
-        const imgURL = '..' + service.ImgURL.slice(5); // Needs revisit after merging admin and client
         mobile ?
             card = `
                 <div class="serviceCard league my-3 d-flex fade-top">
-                    <img class="col-2 col-xl-1" src="${imgURL}" alt="yoga">
+                    <img class="col-2 col-xl-1" src="${service.ImgURL}" alt="yoga">
                     <div class="py-1 col-10 col-xl-11 position-relative d-flex flex-column justify-content-evenly">
                         <div class="px-2 d-flex align-items-center justify-content-between">
                             <h3 class="m-0">${service.Name}</h3>
@@ -38,7 +37,7 @@ async function renderServices() {
             :
             card = `
                 <div class="serviceCard league my-3 d-flex flex-column align-items-center justify-content-between fade-top">
-                    <img class="col-12" src="${imgURL}" alt="yoga">
+                    <img class="col-12" src="${service.ImgURL}" alt="yoga">
                     <div class="col-12 position-relative d-flex flex-column align-items-center justify-content-start">
                         <h3 class="mt-3 align-self-center text-center">${service.Name}</h3>
                         <p class="m-0 text-center">$${service.Price} | ${service.Duration} min</p>
